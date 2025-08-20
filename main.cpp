75ed37d7cab19927ea3725315a67f1348b962839
yun9017 <yun9017ie@naver.com>
2025-08-20 12:29:36 +0900

    02

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char* argv[]) {
-	printf("Hello, Sookmyung Elecetronics!\n");
-	return 0;
-}
