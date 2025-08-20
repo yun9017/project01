c8ea5915553c9d57f0822dc91ff21447c5dbed40
yun9017 <yun9017ie@naver.com>
2025-08-20 12:29:07 +0900

    week02

---
+++ week02/main2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a, b;
+	a = 10;
+	b = 20;
+	
+	printf("%d + %d = %d\n", a, b, a+b);
+	
+	return 0;
+}
